4bdcb01079036ceffa7ee84af5eb127ac4be53b8
alexhill1247 <alexhill1247@gmail.com>
2024-08-02 19:11:50 +1200

    Continued implementation of torrent class.

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -2,10 +2,13 @@ import java.io.File;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -58,6 +61,28 @@ public class Torrent {
     public long downloaded() { return (long) pieceSize * (long) getVerifiedPiecesCount(); }
     public long remaining() { return getTotalSize() - downloaded(); }
 
+    // The last piece may not be the full size
+    public int getPieceSize(int piece) {
+        if (piece == getPieceCount() - 1) {
+            int remainder = (int) (getTotalSize() % pieceSize);
+            if (remainder != 0 ) return remainder;
+        }
+        return pieceSize;
+    }
+
+    public int getBlockCount(int piece) {
+        return (int) Math.ceil((double) getPieceSize(piece) / (double) blockSize);
+    }
+
+    // The last block may not be the full size
+    public int getBlockSize(int piece, int block) {
+        if (block == getBlockCount(piece) - 1) {
+            int remainder = getPieceSize(piece) % blockSize;
+            if (remainder != 0) return remainder;
+        }
+        return blockSize;
+    }
+
     public byte[] infoHash = new byte[20];
     // Maps each value in infoHash into its hexadecimal representation and joins it as a string
     public String getHexStringInfoHash() {
@@ -69,6 +94,20 @@ public class Torrent {
         String infoHashStr = new String(infoHash, StandardCharsets.UTF_8);
         return URLEncoder.encode(infoHashStr, StandardCharsets.UTF_8);
     }
+
+    //TODO peerlist event handler
+
+    private ReentrantLock[] fileWriteLocks;
+    private static MessageDigest md;
+    static {
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //TODO constructor
 }
 
 //TODO fix access modifiers
